9022db3451c49f0b23e773bbcb1b06c2f609cf31
jasontsukamoto <jasontsukamoto@gmail.com>
2015-07-29 12:34:45 -1000

    adds ability to move back to previous state

--- client/views/todo/completed.js
+++ client/views/todo/completed.js
@@ -1,1 +1,11 @@
-completed.js
+Template.completed.events({
+  // 'click .delete' : function() {
+  //   TasksCollection.remove(this._id);
+  // },
+
+  'click .backToWorking' : function() {
+    TasksCollection.update({ _id : this._id }, {
+      $set : { status : 'inProgress' }
+    });
+  }
+});
--- client/views/todo/workingonit.js
+++ client/views/todo/workingonit.js
@@ -4,5 +4,17 @@ Template.workingOnIt.events({
     TasksCollection.update({ _id : this._id }, {
       $set : { status : 'completed' }
     });
+  },
+
+  'click .delete' : function(evt, templ) {
+    evt.preventDefault();
+    TasksCollection.remove(this._id);
+  },
+
+  'click .backToQueue' : function(evt, templ) {
+    evt.preventDefault();
+    TasksCollection.update({ _id : this._id }, {
+      $set : { status : 'default' }
+    });
   }
 });
